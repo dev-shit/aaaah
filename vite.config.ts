758da3077b9347960da3acd75c710b83c4876e5c
Lucas VB <29404838+lucasvbeek@users.noreply.github.com>
2023-04-03 21:56:50 +0200

    Update vite.config.ts

--- vite.config.ts
+++ vite.config.ts
@@ -1,16 +1,10 @@
 import { defineConfig } from "vite";
 import { svelte } from "@sveltejs/vite-plugin-svelte";
-import Unfonts from "unplugin-fonts/vite";
 import Icons from "unplugin-icons/vite";
 
 // https://vitejs.dev/config/
 export default defineConfig({
   plugins: [
-    Unfonts({
-      google: {
-        families: ["Inter", "Unbounded", "Corben"],
-      },
-    }),
     svelte(),
     Icons({
       compiler: "svelte",
